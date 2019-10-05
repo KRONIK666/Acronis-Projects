fd59885adfbd7a123677310cb45805811d4203a0
KRONIK666 <kronik@abv.bg>
2019-10-05 17:50:34 +0300

    Created new Partner Group Tenant

---
+++ AcronisCyberCloudAPI/AcronisCyberCloudAPI/Applications.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace AcronisCyberCloudAPI
+{
+    public class Applications
+    {
+        public class Application
+        {
+            public Item[] items { get; set; }
+
+            public string GetApplicationsInfo(string username, string password)
+            {
+                string url = "https://eu2-cloud.acronis.com:443/api/2/applications";
+                string credentials = username + ":" + password;
+                credentials = Convert.ToBase64String(Encoding.Default.GetBytes(credentials));
+
+                WebRequest request = WebRequest.Create(url);
+                request.Headers["Authorization"] = "Basic " + credentials;
+                request.PreAuthenticate = true;
+                request.Method = "GET";
+                request.ContentType = "application/json";
+
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string responseFromServer = reader.ReadToEnd();
+                response.Close();
+
+                return responseFromServer;
+            }
+        }
+
+        public class Item
+        {
+            public string type { get; set; }
+            public string api_base_url { get; set; }
+            public string name { get; set; }
+            public string[] usages { get; set; }
+            public string id { get; set; }
+        }
+    }
+}
---
+++ AcronisCyberCloudAPI/AcronisCyberCloudAPI/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using static AcronisCyberCloudAPI.Instance;
+using static AcronisCyberCloudAPI.InstanceUser;
+using static AcronisCyberCloudAPI.Tenants;
+
+namespace AcronisCyberCloudAPI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            AdminUser adminUser = new AdminUser();
+            string adminUserInfo = adminUser.GetAdminUserInfo(username, password);
+            adminUser = JsonConvert.DeserializeObject<AdminUser>(adminUserInfo);
+            string rootTenantId = adminUser.tenant_id;
+
+            RootTenant rootTenant = new RootTenant();
+            string rootTenantInfo = rootTenant.GetRootTenant(username, password, rootTenantId);
+            rootTenant = JsonConvert.DeserializeObject<RootTenant>(rootTenantInfo);
+
+            string newTenantJson = File.ReadAllText("templates/tenants.json");
+
+            Tenant partnerTenant = new Tenant();
+            partnerTenant = JsonConvert.DeserializeObject<Tenant>(newTenantJson);
+            partnerTenant.name = "IVAYLO_TSVETKOV";
+            partnerTenant.kind = "partner";
+            partnerTenant.parent_id = rootTenant.items[0].id;
+
+            string postData = JsonConvert.SerializeObject(partnerTenant);
+            partnerTenant.PostTenant(username, password, postData);
+        }
+    }
+}
